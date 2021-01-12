635d8dac59e64490fcc7b5363972e7c6cd40c27b
Angélica Resende <70111126+angelresende@users.noreply.github.com>
2021-01-12 10:10:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
